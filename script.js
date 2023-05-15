f9786d29d9f0e814d185f51a60c15fbeac5a1604
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-05-15 18:25:53 -0300

    ejercicio 1 switch incompleto

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const pPrecioTotal = document.querySelector("#precio-total");
 const selectCantidad = document.querySelector ("select");
-const PRECIO_PRODUCTO_1 = 500;
 
-const olCarrito = document.querySelector ("#carrito")
-const NOMBRE_PROD_1 = "Auto" 
+const PRECIO_PRODUCTO_1 = 500;
+const NOMBRE_PROD_1 = "Auto";
+
+const PRECIO_PRODUCTO_2 = 300;
+const NOMBRE_PROD_2 = "Auto 2";
+
+let precioProductoActual = PRECIO_PRODUCTO_1;
+let nombreProductoActual = NOMBRE_PROD_1; 
+
 
 let precioTotal = 0;
 
+const olCarrito = document.querySelector ("#carrito");
+const fsContenedorProducto = document.querySelector ("#contenedor-producto");
+
+
 function agregarProducto(){
-    precioTotal = (PRECIO_PRODUCTO_1 * selectCantidad.value) + precioTotal;
+    precioTotal = (precioProductoActual * selectCantidad.value) + precioTotal;
     pPrecioTotal.innerHTML = "Total: $" + (precioTotal);
-    olCarrito.innerHTML += `<li> ${NOMBRE_PROD_1} (${selectCantidad.value}) </li>`;
+    olCarrito.innerHTML += `<li> ${nombreProductoActual} (${selectCantidad.value}) </li>`;
 }
+
+function pasarProducto(){
+fsContenedorProducto. innerHTML =`<img src="https://apollo-virginia.akamaized.net/v1/files/ov0b632j9b7x1-AR/image;s=360x0" alt="">
+<p>Auto 2</p>
+<p>Precio : $300</p>
+`;
+}
+
+precioProductoActual = PRECIO_PRODUCTO_2;
+nombreProductoActual = NOMBRE_PROD_2;
+
+
